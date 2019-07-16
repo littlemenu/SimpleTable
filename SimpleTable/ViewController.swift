0eea6c7d3e49c65e0549af5d75aac420cd784f67
littlemenu <jjhisking@gmail.com>
2019-07-16 09:40:41 +0900

    Initial Commit

---
+++ SimpleTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTable
+//
+//  Created by 정재훈 on 16/07/2019.
+//  Copyright © 2019 정재훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
